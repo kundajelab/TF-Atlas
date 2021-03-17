126c215a255e35826d5c9294e64f7ad0f4476c34
zahoorz <zahoor.zafrulla@gmail.com>
2021-03-17 01:57:16 +0000

    update run_pipeline script to include gcp_bucket and pipleine destination to local or gcp

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -11,6 +11,12 @@
 experiment=$1
 encode_access_key=$2
 encode_secret_key=$3
+gcp_bucket=$4 #gbsc-gcp-lab-kundaje-tf-atlas
+pipeline_destination=$5
+
+# create the log file
+logfile=$PWD/$experiment.log
+touch $logfile
 
 # path to the metadata file in the Tf-Atlas folder
 metadata_file_path=$PWD/data/metadata.tsv
@@ -19,10 +25,6 @@ tee -a $logfile
 
 # change directory to the pipeline folder
 cd pipeline
-
-# create the log file
-logfile=$experiment.log
-touch $logfile
 
 # create pipeline params json
 echo $( timestamp ): "
@@ -36,7 +38,7 @@ python \\
     BPNet \\
     one_split.json \\
     chr1 \\
-    bsc-gcp-lab-kundaje-tf-atlas" | tee -a $logfile
+    $gcp_bucket" | tee -a $logfile
     
 python \
     create_pipeline_params_json.py \
@@ -48,17 +50,18 @@ python \
     BPNet \
     one_split.json \
     chr1 \
-    bsc-gcp-lab-kundaje-tf-atlas
+    $gcp_bucket
 
 # if the pipeline params json was generated successfully
 if [ -f pipeline_params_$experiment.json ]
 then
     # run the main pipeline script
-    echo $( timestamp ): "./pipeline.sh" $pipeline_params_$experiment.json \
-    $encode_access_key $encode_secret_key | tee -a $logfile
+    echo $( timestamp ): "./pipeline.sh" pipeline_params_$experiment.json \
+    $encode_access_key $encode_secret_key $pipeline_destination | \
+    tee -a $logfile
     
-#     ./pipeline.sh $pipeline_params_$experiment.json $encode_access_key \
-#     $encode_secret_key
+    ./pipeline.sh pipeline_params_$experiment.json $encode_access_key \
+    $encode_secret_key $pipeline_destination
 else
     echo $( timestamp ): Could not "find" pipeline params json. \
     Aborting. | tee -a $logfile
