2a09abd4aa9fb0e24ede937296ecabf9ae538f11
zahoorz <zahoor.zafrulla@gmail.com>
2021-10-26 21:04:55 +0000

    add encode version parameter

--- kubernetes/outlier_detection/dynamic_yml_submit.py
+++ kubernetes/outlier_detection/dynamic_yml_submit.py
@@ -3,15 +3,17 @@ import sys
 
 template_path = sys.argv[1]
 experiments = sys.argv[2]
+encode_version = sys.argv[3]
 
 # read contents of template file as string
 with open(template_path, 'r') as f:
     template_str = f.read()
-    
+
 # replace '{}' in the template with experiment name
 for experiment in experiments.split():
     yml_str = template_str.replace('{}', experiment.lower())
     yml_str = yml_str.replace('<>', experiment)
+    yml_str = yml_str.replace('[]', encode_version)
     
     # write to new yaml file
     yaml_fname = 'job_{}.yml'.format(experiment)
