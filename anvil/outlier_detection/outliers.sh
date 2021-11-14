6abe9acc4bbedadae97cf8105414b4c95b34d9f4
Vivekanandan Ramalingam <vir@C02FX74SQ05P.local>
2021-11-14 00:57:09 -0800

    create outlier detection

--- anvil/outlier_detection/outliers.sh
+++ anvil/outlier_detection/outliers.sh
@@ -9,12 +9,18 @@ function timestamp {
 }
 
 experiment=$1
-gcp_bucket=$2
-project_dir=$3
+input_outliers_json=$2
+chrom_sizes=$3
+chroms_txt=${4}
+bigwigs=${5}
+peaks=${6}
 
 # create the log file
 logfile=$project_dir/${1}_outliers.log
 touch $logfile
+
+mkdir /project
+project_dir=/project
 
 # create the data directory
 data_dir=$project_dir/data
@@ -26,27 +32,41 @@ reference_dir=$project_dir/reference
 echo $( timestamp ): "mkdir" $reference_dir | tee -a $logfile
 mkdir $reference_dir
 
+
 # copy down data and reference
-echo $( timestamp ): "gsutil cp" gs://$2/data/$1/*.bigWig $data_dir | \
-tee -a $logfile
-gsutil cp gs://$2/data/$1/*.bigWig $data_dir
 
-echo $( timestamp ): "gsutil cp" gs://$2/data/$1/$1.bed.gz $data_dir | \
-tee -a $logfile
-gsutil cp gs://$2/data/$1/$1.bed.gz $data_dir
+echo $( timestamp ): "cp" $chrom_sizes ${reference_dir}/chrom.sizes |\
+tee -a $logfile 
 
-echo $( timestamp ): "gsutil cp" gs://$2/reference/hg38_chroms.txt $reference_dir | \
-tee -a $logfile
-gsutil cp gs://$2/reference/hg38_chroms.txt $reference_dir
+echo $( timestamp ): "cp" $chroms_txt ${reference_dir}/hg38_chroms.txt |\
+tee -a $logfile 
 
-echo $( timestamp ): "gsutil cp" gs://$2/reference/chrom.sizes $reference_dir | \
-tee -a $logfile
-gsutil cp gs://$2/reference/chrom.sizes $reference_dir
+
+# copy down data and reference
+
+cp $chrom_sizes $reference_dir/chrom.sizes
+cp $chroms_txt $reference_dir/hg38_chroms.txt
+
+
+# Step 1: Copy the bigwig and peak files
+
+echo $bigwigs | sed 's/,/ /g' | xargs cp -t $data_dir/
+
+echo $( timestamp ): "cp" $bigwigs ${data_dir}/ |\
+tee -a $logfile 
+
+echo $( timestamp ): "cp" $peaks ${data_dir}/${experiment}.bed.gz |\
+tee -a $logfile 
+
+cp $peaks ${data_dir}/${experiment}.bed.gz
+
 
 # download input json
-echo $( timestamp ): "gsutil cp" gs://$2/input_json/input_outliers.json  $project_dir | \
-tee -a $logfile 
-gsutil cp gs://$2/input_json/input_outliers.json $project_dir/
+
+echo $( timestamp ): "cp" $input_outliers_json \
+$project_dir/input_outliers.json | tee -a $logfile 
+cp $input_outliers_json $project_dir/input_outliers.json
+
 
 # modify the input json for this experiment
 echo  $( timestamp ): "sed -i -e" "s/<>/$1/g" $project_dir/input_outliers.json 
@@ -76,12 +96,3 @@ outliers \
     --blacklist blacklist.bed \
     --global-sample-weight 1.0 \
     --output-bed ${experiment}_inliers.bed 
-
-# copy inliers bed file to gcp
-echo $( timestamp ): "gsutil cp" ${experiment}_inliers.bed  gs://$2/data/$1/ | \
-tee -a $logfile
-gsutil cp ${experiment}_inliers.bed  gs://$2/data/$1/
-
-# copy log file to gcp
-echo $( timestamp ): "gsutil cp" $logfile gs://$2/logs/ | tee -a $logfile 
-gsutil cp $logfile gs://$2/logs/
