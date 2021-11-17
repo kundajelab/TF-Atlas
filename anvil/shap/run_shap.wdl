782e0da7a37548341144101c170c0d856893ba40
Vivekanandan Ramalingam <vir@DNab4215f5.stanford.edu>
2021-11-16 18:15:50 -0800

    add shap to anvil

---
+++ anvil/shap/run_shap.wdl
@@ -0,0 +1,81 @@
+version 1.0
+
+task run_shap {
+	input {
+		String experiment
+		File input_json
+		File reference_file
+		File reference_file_index
+		File chrom_sizes
+		File chroms_txt
+		Array [File] bigwigs
+		File peaks
+
+
+  	}	
+	command {
+		#create data directories and download scripts
+		cd /; mkdir my_scripts
+		cd /my_scripts
+		git clone https://github.com/kundajelab/TF-Atlas.git
+		chmod -R 777 TF-Atlas
+		cd TF-Atlas/anvil/shap/
+
+		##shap
+
+		echo "run /my_scripts/TF-Atlas/anvil/shap/shap_pipeline.sh" ${experiment} ${input_json} ${reference_file} ${reference_file_index} ${chrom_sizes} ${chroms_txt} ${sep=',' bigwigs} ${peaks}
+		/my_scripts/TF-Atlas/anvil/shap/shap_pipeline.sh ${experiment} ${input_json} ${reference_file} ${reference_file_index} ${chrom_sizes} ${chroms_txt} ${sep=',' bigwigs} ${peaks}
+
+		echo "copying all files to cromwell_root folder"
+		
+		cp -r /project/shap /cromwell_root/
+		
+	}
+	
+	output {
+		Array[File] shap = glob("shap/*")
+	
+	
+	}
+
+	runtime {
+		docker: 'vivekramalingam/tf-atlas:gcp-shap'
+		memory: 30 + "GB"
+		bootDiskSizeGb: 100
+		disks: "local-disk 250 HDD"
+		gpuType: "nvidia-tesla-p100"
+		gpuCount: 1
+		nvidiaDriverVersion: "418.87.00" 
+	}
+}
+
+workflow shap {
+	input {
+		String experiment
+		File input_json
+		File reference_file
+		File reference_file_index
+		File chrom_sizes
+		File chroms_txt
+		Array [File] bigwigs
+		File peaks
+
+	}
+
+	call run_shap {
+		input:
+			experiment = experiment,
+			input_json = input_json,
+			reference_file = reference_file,
+			reference_file_index = reference_file_index,	
+			chrom_sizes = chrom_sizes,
+			chroms_txt = chroms_txt,
+			bigwigs = bigwigs,
+			peaks = peaks
+ 	}
+	output {
+		Array[File] shap = run_shap.shap
+
+		
+	}
+}
---
+++ anvil/shap/shap_pipeline.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+function timestamp {
+    # Function to get the current time with the new line character
+    # removed 
+    
+    # current time
+    date +"%Y-%m-%d_%H-%M-%S" | tr -d '\n'
+}
+
+experiment=$1
+input_json=$2
+reference_file=$3
+reference_file_index=$4
+chrom_sizes=$5
+chroms_txt=$6
+bigwigs=$7
+peaks=$8
+
+mkdir /project
+project_dir=/project
+
+# create the log file
+logfile=$project_dir/${1}_shap.log
+touch $logfile
+
+
+# create the data directory
+data_dir=$project_dir/data
+echo $( timestamp ): "mkdir" $data_dir | tee -a $logfile
+mkdir $data_dir
+
+# create the reference directory
+reference_dir=$project_dir/reference
+echo $( timestamp ): "mkdir" $reference_dir | tee -a $logfile
+mkdir $reference_dir
+
+# create the shap directory
+shap_dir=$project_dir/shap
+echo $( timestamp ): "mkdir" $shap_dir | tee -a $logfile
+mkdir $shap_dir
+
+
+# copy down bigwig files, bed file, reference, and model file
+
+
+echo $( timestamp ): "cp" $reference_file ${reference_dir}/hg38.genome.fa | \
+tee -a $logfile 
+
+echo $( timestamp ): "cp" $reference_file_index ${reference_dir}/hg38.genome.fa.fai |\
+tee -a $logfile 
+
+echo $( timestamp ): "cp" $chrom_sizes ${reference_dir}/chrom.sizes |\
+tee -a $logfile 
+
+echo $( timestamp ): "cp" $chroms_txt ${reference_dir}/hg38_chroms.txt |\
+tee -a $logfile 
+
+
+# copy down data and reference
+
+cp $reference_file $reference_dir/hg38.genome.fa
+cp $reference_file_index $reference_dir/hg38.genome.fa.fai
+cp $chrom_sizes $reference_dir/chrom.sizes
+cp $chroms_txt $reference_dir/hg38_chroms.txt
+
+
+# Step 1: Copy the bigwig and peak files
+
+echo $bigwigs | sed 's/,/ /g' | xargs cp -t $data_dir/
+
+echo $( timestamp ): "cp" $bigwigs ${data_dir}/ |\
+tee -a $logfile 
+
+
+echo $( timestamp ): "cp" $peaks ${data_dir}/${experiment}.bed.gz |\
+tee -a $logfile 
+
+cp $peaks ${data_dir}/${experiment}_combined.bed.gz
+
+echo $( timestamp ): "gunzip" ${data_dir}/${experiment}.bed.gz |\
+tee -a $logfile 
+
+gunzip ${data_dir}/${experiment}.bed.gz
+
+
+# download input json template
+# the input json 
+
+echo $( timestamp ): "cp" $input_json \
+$project_dir/input.json | tee -a $logfile 
+cp $input_json $project_dir/input.json
+
+
+# modify the input json for 
+echo  $( timestamp ): "sed -i -e" "s/<>/$1/g" $project_dir/input.json 
+sed -i -e "s/<>/$1/g" $project_dir/input.json | tee -a $logfile 
+
+
+# modify the input json to change the directory name
+echo  $( timestamp ): "sed -i -e" "s/modeling/shap/g" $project_dir/input.json | \
+tee -a $logfile 
+sed -i -e "s/modeling/shap/g" $project_dir/input.json
+
+echo $( timestamp ): "
+shap_scores \\
+    --reference-genome $reference_dir/hg38.genome.fa \\
+    --model $model_dir/${1}_split000.h5 \\
+    --bed-file $data_dir/${1}.bed \\
+    --chroms $(paste -s -d ' ' $reference_dir/hg38_chroms.txt) \\
+    --output-dir $shap_dir \\
+    --input-seq-len 2114 \\
+    --control-len 1000 \\
+    --task-id 0 \\
+    --input-data $project_dir/input.json" | tee -a $logfile
+
+shap_scores \
+    --reference-genome $reference_dir/hg38.genome.fa \
+    --model $model_dir/${1}_split000.h5 \
+    --bed-file $data_dir/${1}.bed \
+    --chroms $(paste -s -d ' ' $reference_dir/hg38_chroms.txt) \
+    --output-dir $shap_dir \
+    --input-seq-len 2114 \
+    --control-len 1000 \
+    --task-id 0 \
+    --input-data $project_dir/input.json # this file doesnt have negatives
