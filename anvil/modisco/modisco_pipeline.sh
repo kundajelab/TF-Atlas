e57453c0ad7c6b7c339fcc75ae27e70fe6a751fc
Vivekanandan Ramalingam <vir@DN2lk5cgr.stanford.edu>
2021-11-20 19:14:26 -0800

    create modisco folder for anvil, shap working with Avs modification

---
+++ anvil/modisco/modisco_pipeline.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+function timestamp {
+    # Function to get the current time with the new line character
+    # removed 
+    
+    # current time
+    date +"%Y-%m-%d_%H-%M-%S" | tr -d '\n'
+}
+
+experiment=$1
+shap=$2
+
+
+
+mkdir /project
+project_dir=/project
+
+# create the log file
+logfile=$project_dir/${1}_modisco.log
+touch $logfile
+
+
+# create the data directory
+data_dir=$project_dir/data
+echo $( timestamp ): "mkdir" $data_dir | tee -a $logfile
+mkdir $data_dir
+
+# create the shap directory
+shap_dir=$project_dir/shap
+echo $( timestamp ): "mkdir" $shap_dir | tee -a $logfile
+mkdir $shap_dir
+
+# create the modisco directories
+modisco_profile_dir=$project_dir/modisco_profile
+echo $( timestamp ): "mkdir" $modisco_profile_dir | tee -a $logfile
+mkdir $modisco_profile_dir
+
+modisco_counts_dir=$project_dir/modisco_counts
+echo $( timestamp ): "mkdir" $modisco_counts_dir | tee -a $logfile
+mkdir $modisco_counts_dir
+
+Step 1: Copy the shap files
+
+echo $( timestamp ): "cp" $shap ${shap_dir}/ |\
+tee -a $logfile 
+
+echo $shap | sed 's/,/ /g' | xargs cp -t $shap_dir/
+
+
+Step 2: Run modisco on counts and profile
+
+echo $( timestamp ): "
+motif_discovery \\
+    --scores-path $shap_dir/profile_scores.h5 \\
+    --output-directory $modisco_profile_dir" | tee -a $logfile
+
+motif_discovery \
+    --scores-path $shap_dir/profile_scores.h5 \
+    --output-directory $modisco_profile_dir
+    
+echo $( timestamp ): "
+motif_discovery \\
+    --scores-path $shap_dir/counts_scores.h5 \\
+    --output-directory $modisco_counts_dir" | tee -a $logfile
+
+motif_discovery \
+    --scores-path $shap_dir/counts_scores.h5 \
+    --output-directory $modisco_counts_dir
+
+
---
+++ anvil/modisco/run_modisco.wdl
@@ -0,0 +1,66 @@
+version 1.0
+
+task run_modisco {
+	input {
+		String experiment
+		Array [File] shap
+
+
+  	}	
+	command {
+		#create data directories and download scripts
+		cd /; mkdir my_scripts
+		cd /my_scripts
+		git clone https://github.com/kundajelab/TF-Atlas.git
+		chmod -R 777 TF-Atlas
+		cd TF-Atlas/anvil/modisco/
+
+		##modisco
+
+		echo "run /my_scripts/TF-Atlas/anvil/modisco/modisco_pipeline.sh" ${experiment} ${sep=',' shap}
+		/my_scripts/TF-Atlas/anvil/modisco/modisco_pipeline.sh ${experiment} ${sep=',' shap}
+
+		echo "copying all files to cromwell_root folder"
+		
+		cp -r /project/modisco /cromwell_root/
+		
+	}
+	
+	output {
+		Array[File] modisco = glob("modisco/*")
+		
+	
+	
+	}
+
+	runtime {
+		docker: 'vivekramalingam/tf-atlas:gcp-modeling'
+		memory: 30 + "GB"
+		bootDiskSizeGb: 100
+		disks: "local-disk 250 HDD"
+		gpuType: "nvidia-tesla-p100"
+		gpuCount: 1
+		nvidiaDriverVersion: "450.51.05" 
+		preemptible: 1
+  		maxRetries: 3
+	}
+}
+
+workflow modisco {
+	input {
+		String experiment
+		Array [File] shap
+
+	}
+
+	call run_modisco {
+		input:
+			experiment = experiment,
+			shap = shap
+ 	}
+	output {
+		Array[File] modisco = run_modisco.modisco
+
+		
+	}
+}
