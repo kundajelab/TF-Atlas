c11b03f42a4d970d66677ab3c45691c968a0af21
Vivekanandan Ramalingam <vir@DN2lk5cs6.stanford.edu>
2021-12-11 01:15:17 -0800

    motif_contributions analysis in Anvil

---
+++ anvil/analysis/analysis_motif_contributions.sh
@@ -0,0 +1,212 @@
+#!/bin/bash
+
+function timestamp {
+    # Function to get the current time with the new line character
+    # removed 
+    
+    # current time
+    date +"%Y-%m-%d_%H-%M-%S" | tr -d '\n'
+}
+
+
+
+experiment=$1
+peaks=$2
+predictions_metrics_test=$3
+predictions_metrics_all=$4
+shap=$5
+modisco_counts=$6
+modisco_profile=$7
+tomtom_database=${8}
+
+analysis_notebooks_dir="/my_scripts/TF-Atlas/anvil/analysis/"
+
+mkdir /project
+project_dir=/project
+
+# create the log file
+logfile=$project_dir/${1}_analysis.log
+touch $logfile
+
+# create the report directories
+analysis_output_dir=$project_dir/analysis
+echo $( timestamp ): "mkdir" $analysis_output_dir | tee -a $logfile
+mkdir $analysis_output_dir
+
+
+# create the data directory
+data_dir=$project_dir/data
+echo $( timestamp ): "mkdir" $data_dir | tee -a $logfile
+mkdir $data_dir
+
+# create the reference directory
+reference_dir=$project_dir/reference
+echo $( timestamp ): "mkdir" $reference_dir | tee -a $logfile
+mkdir $reference_dir
+
+# create the model directory
+model_dir=$project_dir/model
+echo $( timestamp ): "mkdir" $model_dir | tee -a $logfile
+mkdir $model_dir
+
+# create the shap directory
+shap_dir=$project_dir/shap
+echo $( timestamp ): "mkdir" $shap_dir | tee -a $logfile
+mkdir $shap_dir
+
+# create the predictions directories
+predictions_metrics_test_dir=$project_dir/predictions_test
+echo $( timestamp ): "mkdir" $predictions_metrics_test_dir | tee -a $logfile
+mkdir $predictions_metrics_test_dir
+
+predictions_metrics_all_dir=$project_dir/predictions_all
+echo $( timestamp ): "mkdir" $predictions_metrics_all_dir | tee -a $logfile
+mkdir $predictions_metrics_all_dir
+
+# create the modisco directories
+modisco_profile_dir=$project_dir/modisco_profile
+echo $( timestamp ): "mkdir" $modisco_profile_dir | tee -a $logfile
+mkdir $modisco_profile_dir
+
+modisco_counts_dir=$project_dir/modisco_counts
+echo $( timestamp ): "mkdir" $modisco_counts_dir | tee -a $logfile
+mkdir $modisco_counts_dir
+
+# create the tomtom directory
+tomtom_motif_database_dir=$project_dir/tomtom_motif_database
+echo $( timestamp ): "mkdir" $tomtom_motif_database_dir | tee -a $logfile
+mkdir $tomtom_motif_database_dir
+
+tomtom_temp_dir=$project_dir/tomtom_temp_dir
+echo $( timestamp ): "mkdir" $tomtom_temp_dir | tee -a $logfile
+mkdir $tomtom_temp_dir
+
+
+
+# copy down bed file, shap, predictions, modisco files
+
+
+echo $( timestamp ): "cp" $predictions_metrics_test ${predictions_metrics_test_dir}/ |\
+tee -a $logfile 
+
+echo $predictions_metrics_test | sed 's/,/ /g' | xargs cp -t $predictions_metrics_test_dir/
+
+
+echo $( timestamp ): "cp" $predictions_metrics_all ${predictions_metrics_all_dir}/ |\
+tee -a $logfile 
+
+echo $predictions_metrics_all | sed 's/,/ /g' | xargs cp -t $predictions_metrics_all_dir/
+
+
+
+echo $( timestamp ): "cp" $shap ${shap_dir}/ |\
+tee -a $logfile 
+
+echo $shap | sed 's/,/ /g' | xargs cp -t $shap_dir/
+
+
+echo $( timestamp ): "cp" $modisco_counts ${modisco_counts_dir}/ |\
+tee -a $logfile 
+
+echo $modisco_counts | sed 's/,/ /g' | xargs cp -t $modisco_counts_dir/
+
+
+echo $( timestamp ): "cp" $modisco_profile ${modisco_profile_dir}/ |\
+tee -a $logfile 
+
+echo $modisco_profile | sed 's/,/ /g' | xargs cp -t $modisco_profile_dir/
+
+
+tomtom_motif_database_dir
+
+echo $( timestamp ): "cp" $tomtom_database ${tomtom_motif_database_dir}/HOCOMOCO_JASPAR_motifs.txt |\
+tee -a $logfile 
+
+cp $tomtom_database ${tomtom_motif_database_dir}/HOCOMOCO_JASPAR_motifs.txt
+
+
+echo $( timestamp ): "cp" $peaks ${data_dir}/${experiment}.bed.gz |\
+tee -a $logfile 
+
+cp $peaks ${data_dir}/${experiment}.bed.gz
+
+echo $( timestamp ): "gunzip" ${data_dir}/${experiment}.bed.gz |\
+tee -a $logfile 
+
+gunzip ${data_dir}/${experiment}.bed.gz
+
+peaks_path=${data_dir}/${experiment}.bed
+
+# download input json template
+# the input json 
+
+
+
+#motif contributions results
+for key in profile counts
+do
+    # temp directory for tomtom matching
+    echo $( timestamp ): "mkdir" $tomtom_temp_dir/$key | tee -a $logfile
+    mkdir $tomtom_temp_dir/$key
+    
+	TFM_PRED_PATH=$predictions_metrics_all_dir/${experiment}_split000_predictions.h5 \
+		TFM_SHAP_PATH=$shap_dir/${key}_scores.h5 \
+		TFM_TFM_PATH=$project_dir/modisco_${key}/modisco_results.h5 \
+		TFM_PEAKS_PATH=$peaks_path \
+        TFM_TOMTOM_DB_PATH=${tomtom_motif_database_dir}/HOCOMOCO_JASPAR_motifs.txt \
+        TFM_TOMTOM_TEMP_DIR=$tomtom_temp_dir/$key \
+        TFM_KEY=$key \
+        TFM_analysis_DIR=$analysis_output_dir \
+		jupyter nbconvert \
+        --execute $analysis_notebooks_dir/motif_contributions.ipynb \
+        --to HTML --output $analysis_output_dir/${key}_tfm_results \
+        --ExecutePreprocessor.timeout=-1 &
+done
+wait
+
+# #Examples of seqlets and profile predictions
+# for key in profile counts
+# do
+# 	TFM_PRED_PATH=$predictions_path \
+# 		TFM_SHAP_PATH=$shap_dir/${key}_scores.h5 \
+# 		TFM_TFM_PATH=$modisco_dir/$key/modisco_results.h5 \
+# 		jupyter nbconvert \
+#         --execute $analysis_notebooks_dir/showcase_motifs_and_profiles.ipynb \
+#         --to HTML --output $analysis_output_dir/${key}_seqlet_profile_examples \
+#         --ExecutePreprocessor.timeout=-1 &
+# done
+
+# #Motif hits (this runs MOODS)
+# for key in profile counts
+# do
+# 	TFM_TFM_PATH=$modisco_dir/$key/modisco_results.h5 \
+# 		TFM_SHAP_PATH=$shap_dir/${key}_scores.h5 \
+# 		TFM_PEAKS_PATH=$peaks_path \
+# 		TFM_MOODS_DIR=$moods_dir/$key \
+#         TFM_REFERENCE_PATH=$reference_dir/genome.fa \
+# 		jupyter nbconvert \
+#         --execute $analysis_notebooks_dir/summarize_motif_hits.ipynb --to HTML \
+#         --output $analysis_output_dir/${key}_motif_hits \
+#         --ExecutePreprocessor.timeout=-1 &
+# done
+# wait
+
+# # Clustering of motif hits and peaks
+# for key in profile counts
+# do
+#     # cache directory for cluster motifs analysis notebook
+#     cluster_cache_dir=$analysis_output_dir/cluster_cache_$key
+#     echo $( timestamp ): "mkdir" $cluster_cache_dir | tee -a $logfile
+#     mkdir $cluster_cache_dir
+
+# 	TFM_TFM_PATH=$modisco_dir/$key/modisco_results.h5 \
+# 		TFM_SHAP_PATH=$shap_dir/${key}_scores.h5 \
+# 		TFM_MOODS_DIR=$moods_dir/$key \
+# 		TFM_EMB_PATH=$embeddings_path \
+#         TFM_CLUSTER_CACHE=$cluster_cache_dir \
+# 		jupyter nbconvert \
+#         --execute $analysis_notebooks_dir/cluster_motif_hits_and_peaks.ipynb \
+#         --to HTML --output $analysis_output_dir/${key}_motif_peak_clustering \
+#         --ExecutePreprocessor.timeout=-1 &
+# done
+# wait
---
+++ anvil/analysis/run_analysis_motif_contributions.wdl
@@ -0,0 +1,93 @@
+version 1.0
+
+task run_reports {
+	input {
+		String experiment
+		File peaks
+		Array [File] predictions_test_chrom
+		Array [File] predictions_all_chrom
+		Array [File] shap
+		Array [File] modisco_counts
+		Array [File] modisco_profile
+		File tomtom_database
+
+
+  	}	
+	command {
+		#create data directories and download scripts
+		cd /; mkdir my_scripts
+		cd /my_scripts
+		git clone https://github.com/kundajelab/TF-Atlas.git
+		chmod -R 777 TF-Atlas
+		cd TF-Atlas/anvil/analysis/
+
+		##shap
+
+		echo "run /my_scripts/TF-Atlas/anvil/analysis/analysis_motif_contributions.sh" ${experiment} ${peaks} ${sep=',' predictions_test_chrom} ${sep=',' predictions_all_chrom} ${sep=',' shap} ${sep=',' modisco_counts} ${sep=',' modisco_profile} ${tomtom_database}
+		/my_scripts/TF-Atlas/anvil/analysis/analysis_motif_contributions.sh ${experiment} ${peaks} ${sep=',' predictions_test_chrom} ${sep=',' predictions_all_chrom} ${sep=',' shap} ${sep=',' modisco_counts} ${sep=',' modisco_profile} ${tomtom_database}
+
+		echo "copying all files to cromwell_root folder"
+		
+		cp -r /project/analysis /cromwell_root/
+
+		ls /project/analysis
+
+		
+	}
+	
+	output {
+
+
+		String counts_motif1 = read_string("reports/counts_motif_contributions.txt")
+
+		String profile_motif1 = read_string("reports/profile_motif_contributions.txt")
+
+
+		Array[File] reports = glob("reports/*")
+		
+	
+	
+	}
+
+	runtime {
+		docker: 'vivekramalingam/tf-atlas:gcp-reports'
+		memory: 20 + "GB"
+		bootDiskSizeGb: 40
+		disks: "local-disk 50 HDD"
+  		maxRetries: 1
+	}
+}
+
+workflow reports {
+	input {
+		String experiment
+		File peaks
+		Array [File] predictions_test_chrom
+		Array [File] predictions_all_chrom
+		Array [File] shap
+		Array [File] modisco_counts
+		Array [File] modisco_profile
+		File tomtom_database
+
+	}
+
+	call run_reports {
+		input:
+			experiment = experiment,
+			peaks = peaks,
+			predictions_test_chrom = predictions_test_chrom,
+			predictions_all_chrom = predictions_all_chrom,
+			shap = shap,	
+			modisco_counts = modisco_counts,
+			modisco_profile = modisco_profile,
+			tomtom_database = tomtom_database,
+ 	}
+	output {
+
+		String counts_motif1 = run_reports.counts_motif_contributions
+
+		String profile_motif1 = run_reports.profile_contributions
+
+
+	}
+}
